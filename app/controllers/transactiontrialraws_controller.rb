19954129c933f474c0d5311c5d682f1981a6cbc1
Owen Ayora <ayoraowen@gmail.com>
2026-04-30 19:47:04 +0300

    created transactiontrialsraws_controller and added logic

---
+++ app/controllers/transactiontrialraws_controller.rb
@@ -0,0 +1,21 @@
+class TransactiontrialrawsController < ApplicationController
+  
+    skip_before_action :verify_authenticity_token
+    
+    def create
+        # Read raw JSON body (important for webhooks)
+        raw = request.body.read
+        data = JSON.parse(raw) rescue params
+    
+        transactiontrialraw = Transactiontrialraw.new(
+            payload: data.to_s
+        )
+        if transactiontrialraw.save
+            render json: { message: "Transaction trial raw created successfully" }, status: :created
+        else
+            render json: { errors: transactiontrialraw.errors.full_messages }, status: :unprocessable_entity
+        end
+    end
+
+end
+
